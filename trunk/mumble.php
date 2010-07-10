b095bb78463f12a2f1de643bd9d4a60288883219
Pimmetje@gmail.com <Pimmetje@gmail.com@6e924d06-d379-60d8-f45c-8c595e57c962>
2010-07-10 09:37:00 +0000

    Fist basic version

---
+++ trunk/mumble.php
@@ -0,0 +1,16 @@
+<?php
+//include the parser
+require('MumbleReader.class.php');
+// GET
+$port = (isset($_GET['port']))?filter_input(INPUT_GET, 'port', FILTER_VALIDATE_INT):0;
+
+$mReader = new MumbleReader($port);
+
+//Make sure page is not cached
+header ("Expires: Thu, 17 May 2001 10:17:17 GMT");    // Date in the past
+header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+header ("Cache-Control: no-cache, must-revalidate");  // HTTP/1.1
+header ("Pragma: no-cache");                          // HTTP/1.0
+//header ("Content-type: application/json"); //Not sure if this is nice because sometimes it will return json but for the rest it returns javascrpt
+//output the json
+echo $mReader->render();
